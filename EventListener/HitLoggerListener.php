973996efede9107f384555b9885b8e1a66ce098a
Yoan Arnaudov <jonidev@gmail.com>
2017-10-10 11:37:36 +0300

    log logger time

--- EventListener/HitLoggerListener.php
+++ EventListener/HitLoggerListener.php
@@ -7,11 +7,21 @@ use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 class HitLoggerListener {
     function __construct($hitLogger) {
         $this->hitLogger = $hitLogger;
+
+        $this->logTimeMS = 0;
     }
 
     public function onKernelRequest(GetResponseEvent $event) {
         $request = $event->getRequest();
 
+        $start = microtime(true);
+
         $this->hitLogger->logHit($request);
+
+        $this->logTimeMS =  microtime(true) - $start;
+    }
+
+    public function getLogTime() {
+        return $this->logTimeMS;
     }
 }
